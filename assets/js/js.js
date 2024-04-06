2bdd9ba37377d5a72ed1c8f5d79726952c9032f7
doanduyvan <duyvanlee2001@gmail.com>
2024-04-06 15:49:03 +0700

    up

--- assets/js/js.js
+++ assets/js/js.js
@@ -3,6 +3,8 @@ const btn_menu = document.getElementById('btn_menu');
 const nav = document.getElementById('nav');
 const close1 = document.getElementById('close');
 const overlay = document.getElementById('overlay');
+const arrA = document.querySelectorAll('#nav ul li a');
+
 
 btn_menu.addEventListener('click',()=>{
     nav.classList.toggle('toggle');
@@ -16,3 +18,21 @@ overlay.addEventListener('click',()=>{
     nav.classList.toggle('toggle');
     overlay.classList.toggle('toggle');
 })
+
+
+const home = document.getElementById('header');
+
+
+document.addEventListener('scroll',()=>{
+   home.classList.toggle('active', window.scrollY > 100);
+    console.log(window.scrollY);
+
+}) 
+
+arrA.forEach(item=>{
+    item.addEventListener('click',()=>{
+        nav.classList.remove('toggle');
+        overlay.classList.remove('toggle');
+    })
+})
+
